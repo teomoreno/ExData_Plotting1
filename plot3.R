f491e008a3b5256a84691d25b9fe0cf46af16cc6
teomoreno <teo.moreno@gmail.com>
2015-02-08 16:16:35 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,11 +1,11 @@
 # Generating Plot 3
 
 with(data_sub, {
-    plot(Sub_metering_1 ~ Date_time, type = "l", 
-    ylab = "Global Active Power (kilowatts)", xlab = "")
+    plot(Sub_metering_1 ~ Date_time, ylab = "Global Active Power (kilowatts)", xlab = "", type = "l")
     lines(Sub_metering_2 ~ Date_time, col = 'Red')
     lines(Sub_metering_3 ~ Date_time, col = 'Blue')
-})
+	})
+
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
