6926d4370c067d706715fa5f21146415310a75ac
teomoreno <teo.moreno@gmail.com>
2015-02-08 13:39:56 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
 # Generating Plot 2 
 
 plot(data_sub$Global_active_power ~ data$Date_time, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png,"plot2.png", width = 480, height = 480)
+dev.off()
