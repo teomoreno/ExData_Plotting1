abc31f5a51cfcccfebf2c16138f8a3b89348aeac
teomoreno <teo.moreno@gmail.com>
2015-02-08 13:46:23 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,3 +8,6 @@ with(data_sub, {
 })
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png,"plot3.png", width = 480, height = 480)
+dev.off()
