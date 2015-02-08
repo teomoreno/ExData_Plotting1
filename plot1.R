d03f55f6c66e347c5000ea624872d5167abb338b
teomoreno <teo.moreno@gmail.com>
2015-02-08 13:46:53 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
 # Generating Plot 1
 
 hist(data_sub$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)") 
+
 dev.copy(png,"plot1.png", width = 480, height = 480)
 dev.off()
